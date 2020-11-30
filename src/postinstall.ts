10a1e68048bf43bde493b4d3b313f4f2fc21437b
iiAku <ygendrey@gmail.com>
2020-11-30 23:21:45 +0000

    modify postinstall

---
+++ src/postinstall.ts
@@ -0,0 +1,25 @@
+import * as fs from 'fs'
+import * as path from 'path'
+if (process.env.NODE_ENV === 'production') {
+  const parent = __dirname.split('/').slice(0, -1).join('/')
+  const dirs = fs.readdirSync(parent)
+  const keep = new Set(['dist', 'prisma', 'node_modules', 'Procfile'])
+
+  const isDirectory = (element: string) => fs.lstatSync(element).isDirectory()
+  const isFile = (element: string) => fs.lstatSync(element).isFile()
+
+  for (const dir of dirs) {
+    const element = path.join(parent, dir)
+    if (isDirectory(element) && !keep.has(dir)) {
+      console.log('removing dir', element)
+      fs.rmdirSync(element)
+      continue
+    }
+
+    if (isFile(element) && !keep.has(dir)) {
+      console.log('removing file', element)
+      fs.unlinkSync(element)
+      continue
+    }
+  }
+}
