2e5b28cb50ac1c9c2ff51b151d5145e2af05abc8
Yoann Gendrey <ygendrey@gmail.com>
2020-12-18 16:41:37 +0100

    comment out dev seed data

--- infra/docker_postgres_init.sql
+++ infra/docker_postgres_init.sql
@@ -113,29 +113,29 @@ CREATE TABLE "public"."options" (
 );
 
 
-INSERT INTO public."Organization" (id,email,"password","createdAt") VALUES 
-('e9d0f049-8c95-4489-b224-0390fc7df207','bob@bob.com','$2b$10$iq.xZjVzAUSd5C2YPBh4gO6AHOdwukAOvLG//d5UAaIv1xgFiKzRO','2020-12-15 20:28:39.886')
-;
+-- INSERT INTO public."Organization" (id,email,"password","createdAt") VALUES 
+-- ('e9d0f049-8c95-4489-b224-0390fc7df207','bob@bob.com','$2b$10$iq.xZjVzAUSd5C2YPBh4gO6AHOdwukAOvLG//d5UAaIv1xgFiKzRO','2020-12-15 20:28:39.886')
+-- ;
 
-INSERT INTO public."Auth" (id,"organizationId",ip,"createdAt","updatedAt") VALUES 
-('8bd87d76-4358-45d8-a803-7be6e91540e1','e9d0f049-8c95-4489-b224-0390fc7df207','127.0.0.1','2020-12-15 20:29:05.024','2020-12-15 20:29:05.024')
-;
+-- INSERT INTO public."Auth" (id,"organizationId",ip,"createdAt","updatedAt") VALUES 
+-- ('8bd87d76-4358-45d8-a803-7be6e91540e1','e9d0f049-8c95-4489-b224-0390fc7df207','127.0.0.1','2020-12-15 20:29:05.024','2020-12-15 20:29:05.024')
+-- ;
 
-INSERT INTO public."MenuCategory" (id,"organizationId","name") VALUES 
-('ae8ce485-31d5-47be-b1f2-e57ebd132092','e9d0f049-8c95-4489-b224-0390fc7df207','Boissons')
-,('219c601b-ff38-4108-86e3-4ff0f7a82734','e9d0f049-8c95-4489-b224-0390fc7df207','Tapas')
-;
+-- INSERT INTO public."MenuCategory" (id,"organizationId","name") VALUES 
+-- ('ae8ce485-31d5-47be-b1f2-e57ebd132092','e9d0f049-8c95-4489-b224-0390fc7df207','Boissons')
+-- ,('219c601b-ff38-4108-86e3-4ff0f7a82734','e9d0f049-8c95-4489-b224-0390fc7df207','Tapas')
+-- ;
 
-INSERT INTO public."MenuProduct" (id,"organizationId","name",description) VALUES 
-('07f7b05b-aec2-4848-a715-46883bde8c72','e9d0f049-8c95-4489-b224-0390fc7df207','Coca','Boison au cola')
-,('65f52058-3576-4211-9c12-f18c655456d3','e9d0f049-8c95-4489-b224-0390fc7df207','Fanta','Boison à l''orange')
-;
+-- INSERT INTO public."MenuProduct" (id,"organizationId","name",description) VALUES 
+-- ('07f7b05b-aec2-4848-a715-46883bde8c72','e9d0f049-8c95-4489-b224-0390fc7df207','Coca','Boison au cola')
+-- ,('65f52058-3576-4211-9c12-f18c655456d3','e9d0f049-8c95-4489-b224-0390fc7df207','Fanta','Boison à l''orange')
+-- ;
 
-INSERT INTO public."Menu" (id,"organizationId","name") VALUES 
-('78daa9fa-551a-40f3-a060-807315f551eb','e9d0f049-8c95-4489-b224-0390fc7df207','L''utime carte des boissons happy hour')
-;
+-- INSERT INTO public."Menu" (id,"organizationId","name") VALUES 
+-- ('78daa9fa-551a-40f3-a060-807315f551eb','e9d0f049-8c95-4489-b224-0390fc7df207','L''utime carte des boissons happy hour')
+-- ;
 
-INSERT INTO public."MenuProductOption" (id,"organizationId",description) VALUES 
-('1673d0cd-04dc-44b3-94a3-6894e0ce2358','e9d0f049-8c95-4489-b224-0390fc7df207','1L')
-,('1673d0cd-04dc-44b3-94a3-6894e0ce2359','e9d0f049-8c95-4489-b224-0390fc7df207','1/2L')
-;
+-- INSERT INTO public."MenuProductOption" (id,"organizationId",description) VALUES 
+-- ('1673d0cd-04dc-44b3-94a3-6894e0ce2358','e9d0f049-8c95-4489-b224-0390fc7df207','1L')
+-- ,('1673d0cd-04dc-44b3-94a3-6894e0ce2359','e9d0f049-8c95-4489-b224-0390fc7df207','1/2L')
+-- ;
